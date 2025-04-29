5c2bb41ec73497fcd3c86389f240e0598eb65ddf
bc-justin <b.justin38@yahoo.com>
2025-04-29 19:39:32 +0300

    did everything v1.2

--- app/src/main/java/com/example/mobile_project/SecondSplashActivity.java
+++ app/src/main/java/com/example/mobile_project/SecondSplashActivity.java
@@ -4,10 +4,13 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,7 +25,10 @@ public class SecondSplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second_splash);
-        TextView quoteTextView = findViewById(R.id.quoteTextView);
+        TextView quoteTextView = findViewById(R.id.quoteText);
+        CardView cardView = findViewById(R.id.cardView);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        cardView.startAnimation(fadeIn);
         QuoteManager.fetchRandomQuoteForSplash(this, quoteTextView);
         new Handler().postDelayed(new Runnable() {
             @Override
